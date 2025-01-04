2e1342f0b28a20702943840abd5af2c3e34de609
Sakib Al Mahamud <144713331+34Sakib@users.noreply.github.com>
2025-01-04 14:32:01 +0600

    Add files via upload

---
+++ string.php
@@ -0,0 +1,22 @@
+<?php
+
+$strings = ["Hello", "World", "PHP", "Programming"];
+
+foreach ($strings as $string) {
+
+    $vowelCount = 0;
+    $vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']; 
+    for ($i = 0; $i < strlen($string); $i++) {
+        if (in_array($string[$i], $vowels)) {
+            $vowelCount++;
+        }
+    }
+
+    $reversedString = strrev($string);
+
+    echo "Original String: $string, " ;
+    echo "Vowel Count: $vowelCount, " ;
+    echo "Reversed String: $reversedString" ."<br>";
+}
+
+?>
